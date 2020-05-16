f397fb165f129998e615090a8b0ef38dcfc547b6
Cola Chen <colachg@gmail.com>
2020-05-16 10:21:49 +0800

    Add support for arm64

--- build.sh
+++ build.sh
@@ -19,6 +19,7 @@ then
     rm -rf /tmp/repo/2.31-r0/glibc-2.31-r0.apk
 fi
 
+# RaspberryPi 3B+
 if [ `uname -m` = armv7l ]
 then
     KCP_URL=https://github.com/xtaci/kcptun/releases/download/v${KCP_VER}/kcptun-linux-arm7-${KCP_VER}.tar.gz
@@ -33,3 +34,17 @@ then
     rm -rf trojan-go-linux-arm.zip trojan-go
 fi
 
+# RaspberryPi 4
+if [ `uname -m` = aarch64 ]
+then
+    KCP_URL=https://github.com/xtaci/kcptun/releases/download/v${KCP_VER}/kcptun-linux-arm64-${KCP_VER}.tar.gz
+#     HTTP_OVER_SOCKS_URL=https://github.com/colachg/http-over-socks/releases/download/v${HTTP_OVER_SOCKS_VER}/http-over-socks_${HTTP_OVER_SOCKS_VER}_linux_armv7.tar.gz
+    TROJAN_GO_URL=https://github.com/p4gefau1t/trojan-go/releases/download/v${TROJAN_GO_VER}/trojan-go-linux-arm64.zip
+    curl -sSL $KCP_URL | tar xz -C /usr/bin/ client_linux_arm64
+#     curl -sSL $HTTP_OVER_SOCKS_URL| tar xz -C /usr/bin/ httpget
+    wget $TROJAN_GO_URL
+    mkdir trojan-go
+    unzip trojan-go-linux-arm64.zip -d trojan-go
+    mv trojan-go/trojan-go trojan-go/geoip.dat trojan-go/geosite.dat /usr/bin/
+    rm -rf trojan-go-linux-arm64.zip trojan-go
+fi
